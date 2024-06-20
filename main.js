3af2c1dc564fd3f5a56be139c6b38336b81b6d7a
ponziot <111065521+ponziot@users.noreply.github.com>
2024-06-20 09:41:20 -0300

    20/06

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-const botoes = document.querySelectorAll(".botao")
-const conteudo = document.querySelectorAll(".aba-conteudo")
+const botoes = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
+contador[0].textContent = "Olá"
+const tempoObjetivo1 = new Date(2024,11,31,23,59)
+const tempoObjetivo2 = new Date(2024,11,31,23,59)
+const tempoObjetivo3 = new Date(2024,11,31,23,59)
+const tempoObjetivo4 = new Date(2024,11,31,23,59)
+let agora = new Date();
+contador[0].textContent = tempoObjetivo1-agora
+contador[1].textContent = tempoObjetivo2-agora
+contador[2].textContent = tempoObjetivo3-agora
+contador[3].textContent = tempoObjetivo4-agora
 
-for (let i = 0; i < botoes.length; i++){
-    botoes[i].onclick = function(){
-      for (let j = 0; j < botoes.length; j++){
-           botoes[j].classList.remove("ativo")
-           conteudo[j].classList.remove("ativo")
-      }
-      botoes[i].classList.add("ativo")
-      conteudo[i].classList.add("ativo")
-  }
+for (let i = 0 ; i < botoes.length ; i++){
+    botoes[i].onclick = function (){
+        for (let j = 0; j < botoes.length ; j++){
+        botoes[j].classList.remove("ativo");
+        conteudo[j].classList.remove('ativo')
+        }
+        botoes[i].classList.add("ativo");
+        conteudo[i].classList.add("ativo")
+    }
 }
